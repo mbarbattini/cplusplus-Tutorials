62d285a752f7ba418640204cc0d0ec196e37f2da
mbarbattini <mbarbattini@gmail.com>
2020-06-10 18:10:12 -0600

    added files

---
+++ tutorialPart2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <climits>
+using std::cout;
+using std::endl;
+
+int main()
+{
+  short a;
+  int x;
+  long c;
+  long long d;
+  //short <= int <= long <= long long
+  unsigned short aa;
+  unsigned int bb;
+  unsigned long cc;
+  unsigned long long dd;
+
+  cout << SHRT_MAX << endl;
+  cout << SHRT_MIN << endl;
+  cout << USHRT_MAX << endl;
+  cout << INT_MAX << endl;
+  cout << LLONG_MAX << endl;
+}
