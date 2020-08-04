645a5d6fbd2822f8026c23a3d11cacd8abce3957
Matthew Barbattini <mbarbattini@gmail.com>
2020-08-04 17:30:42 -0600

    finished loops

--- loops.cpp
+++ loops.cpp
@@ -3,11 +3,31 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int main () {
-    int fact = 5;
-    int factorial = fact;
-    for (int i = fact - 1; i > 0; i--) {
+// int main () {
+//     int fact = 5;
+//     int factorial = fact;
+//     for (int i = fact - 1; i > 0; i--) {
+//         factorial *= i;
+//     }
+//     cout << "factorial of " << fact << ": " << factorial << endl;
+// }
+
+// //while loop for finite loops
+// int main() {
+//     int i = 0;
+//     while(i < 10) {
+//         //action
+//         i++;
+//     }
+// }
+
+// while loop to calculate factorial
+int main() {
+    int factorial = 10;
+    int i = factorial - 1;
+    while(i > 1) {
         factorial *= i;
+        i--;
     }
-    cout << "factorial of " << fact << ": " << factorial << endl;
+    cout << factorial << endl;
 }
